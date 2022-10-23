105323e0e2e528dae8427e56d364abf0fbdba88c
James Njenga <knjenga94@gmail.com>
2022-10-23 19:56:38 +0300

    Completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,28 @@
 class Dog
+    def name=(name)
+        @name=name
+    end
+
+    def name
+        @name
+    end
+    
+    def breed=(breed)
+        @breed=breed
+    end
+    
+    def breed
+        @breed
+    end
 
 end
+
+dog1=Dog.new
+dog1.name="Tommy"
+dog1.breed="Mastiff"
+
+dog2 =Dog.new
+dog2.name="Maxwell"
+dog2.breed="Chihuahua"
+puts "Iam dog 1: My name is #{dog1.name} and I am a #{dog1.breed}"
+puts "Iam dog 2: My name is #{dog2.name} and I am a #{dog2.breed}"
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,24 @@
 class Person
+   def name=(name)
+    @name=name
+   end
+
+   def name
+    @name
+   end
+   
+   def job=(job)
+    @job=job
+   end
+
+   def job
+    @job
+   end
 
 end
+
+james=Person.new
+james.name="James"
+james.job="Software Engineer"
+
+puts "Hello my name is #{james.name} and I am a #{james.job}"
